38eb96238b73d0d81d3fbcbd3704c7b63c6c6487
JulianLeon <jslp0808@gmail.com>
2025-06-18 15:10:49 -0500

    Se actualiza el texto del print

--- tutorial.java
+++ tutorial.java
@@ -1,6 +1,6 @@
 public class tutorial {
    public static void main(String[] args) {
-      System.out.println("Hola mundo");
+      System.out.println("Estoy haciendo cambios correctivos");
       
    }
 }
