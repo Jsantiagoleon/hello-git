d610114e009cb777961f156b42047d78ce9f666b
danieleon08 <dflp2005@gmail.com>
2025-06-20 17:53:49 -0500

    agrego segundo oso ñai

--- login.java
+++ login.java
@@ -2,6 +2,7 @@ public class login {
    public static void main(String[] args) {
       System.out.println("Login");
       System.out.println("Oso no se ñai ");
-      
+      System.out.println("Oso no se ñai " + "Oso no se ñai");
+
    }
 }
